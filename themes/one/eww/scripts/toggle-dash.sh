25220d8065531e59e37200a115d8423839f3bdea
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-09-11 17:11:42 -0500

    new widget added

--- themes/one/eww/scripts/toggle-dash.sh
+++ themes/one/eww/scripts/toggle-dash.sh
@@ -7,18 +7,24 @@ if [[ "$1" == "--keypress" && "$reserves" == "[0,0,0,50]" ]]; then
     exit
 fi
 
-state=$(eww get rev_dash)
+state=$(eww -c $HOME/.config/hypr/themes/one/eww/ get rev_dash)
 
 if [[ "$state" == "true" || "$1" == "--close" ]]; then
-    eww update anim_open_dash=false
-    eww update rev_dash=false
+    # eww update anim_open_dash=false
+    eww -c $HOME/.config/hypr/themes/one/eww/ update anim_open_dash=false 
+    # eww update rev_dash=false
+    eww -c $HOME/.config/hypr/themes/one/eww/ update rev_dash=false 
     sleep 0.08
-    eww close dashboard 2>/dev/null
+    # eww close dashboard 2>/dev/null
+    eww -c $HOME/.config/hypr/themes/one/eww/ close dashboard0 2>/dev/null 
 else
     # scripts/toggle-overview.sh --close &
     # scripts/toggle-osettings.sh --close &
     # scripts/toggle-onotify.sh --close &
-    eww update anim_open_dash=true
-    eww open dashboard
-    eww update rev_dash=true
+    # eww update anim_open_dash=true
+    eww -c $HOME/.config/hypr/themes/one/eww/ update anim_open_dash=true 
+    # eww open dashboard
+    eww -c $HOME/.config/hypr/themes/one/eww/ open dashboard0
+    # eww update rev_dash=true
+    eww -c $HOME/.config/hypr/themes/one/eww/ update rev_dash=true
 fi
