662448c5f408123502d3f50af1401e5a1593ef8e
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2023-12-14 14:47:24 -0500

    date button

--- dots/ags/js/bar/buttons/DateButton.js
+++ dots/ags/js/bar/buttons/DateButton.js
@@ -2,7 +2,7 @@ import App from 'resource:///com/github/Aylur/ags/app.js';
 import Clock from '../../misc/Clock.js';
 import PanelButton from '../PanelButton.js';
 
-export default ({ format = '%H:%M %A,%e %b' } = {}) => PanelButton({
+export default ({ format = '%H:%M 󰧟 %e %a %b' } = {}) => PanelButton({
     class_name: 'dashboard panel-button',
     on_clicked: () => App.toggleWindow('dashboard'),
     window: 'dashboard',
