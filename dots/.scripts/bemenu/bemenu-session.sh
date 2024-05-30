4b3a55a78f56e29183982cb48c26f6c42e1aa3b5
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-05-30 16:06:49 -0500

    Update workspace windowrules in zellij sessions

--- dots/.scripts/bemenu/bemenu-session.sh
+++ dots/.scripts/bemenu/bemenu-session.sh
@@ -6,9 +6,8 @@
 session=$(zellij ls | cut -c 8- | cut -d " " -f1 | sed -r 's/\x1B\[[0-9;]*[mK]//' | bemenu -i --prompt="󱊄 Select a session: ")
 
 if [ -n "$session" ]; then
-    hyprctl dispatch workspace 3
     foot --title=DotFile -e zellij a "$session" &> /dev/null &
-    hyprctl dispatch fullscreen 0
-    hyprctl dispatch fullscreen 0
+    # hyprctl dispatch fullscreen 0
+    # hyprctl dispatch fullscreen 0
 fi
 
