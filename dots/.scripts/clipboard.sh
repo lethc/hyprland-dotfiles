e273c39fcd420c5a1f64641d03ea549b21c9470c
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-05-07 15:56:32 -0500

    rename some scripts

--- dots/.scripts/clipboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true; do
-    wl-paste | xclip -selection clipboard
-    xclip -selection clipboard -o | wl-copy
-    sleep 0.5
-done
--- dots/.scripts/gdExtract.py
+++
@@ -1,7 +0,0 @@
-import sys, re
-
-link = str(sys.argv[1])
-
-# extracts the unique ID of the image/file
-x = re.search(r"\bd\/\w+[^/]([A-Za-z0-9-_])*", link)
-print("https://drive.google.com/uc?export=view&id="+x.group()[2:])
